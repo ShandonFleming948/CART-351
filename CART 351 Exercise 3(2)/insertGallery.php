a472de83983f983889f1b9dbade6d400ef5d8dd6
ShandonFleming948 <shandonflem@gmail.com>
2021-11-20 10:16:42 -0500

    Exercise 3
    Customized the code

--- CART 351 Exercise 3(2)/insertGallery.php
+++ CART 351 Exercise 3(2)/insertGallery.php
@@ -50,14 +50,14 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
 <!--form done using more current tags... -->
 <form id="insertGallery" action="" enctype ="multipart/form-data">
 <!-- group the related elements in a form -->
-<h3> SUBMIT AN ART WORK :::</h3>
+<h3> Agenda/To Do List :::</h3>
 <fieldset>
-<p><label>Artist:</label><input type="text" size="24" maxlength = "40" name = "a_name" required></p>
-<p><label>Title:</label><input type = "text" size="24" maxlength = "40"  name = "a_title" required></p>
-<p><label>Geographic Location:</label><input type = "text" size="24" maxlength = "40" name = "a_geo_loc" required></p>
-<p><label>Creation Date (DD-MM-YYYY):</label><input type="date" name="a_date" required></p>
-<p><label>Description:</label><textarea type = "text" rows="4" cols="50" name = "a_descript" required></textarea></p>
-<p><label>Upload Image:</label> <input type ="file" name = 'filename' size=10 required/></p>
+<p><label>Course/Class:</label><input type="text" size="24" maxlength = "40" name = "a_name" required></p>
+<p><label>Teacher:</label><input type = "text" size="24" maxlength = "40"  name = "a_title" required></p>
+<p><label>Classroom:</label><input type = "text" size="24" maxlength = "40" name = "a_geo_loc" required></p>
+<p><label>Due Date (DD-MM-YYYY):</label><input type="date" name="a_date" required></p>
+<p><label>Description of Assignment:</label><textarea type = "text" rows="4" cols="50" name = "a_descript" required></textarea></p>
+<p><label>Upload Assignment:</label> <input type ="file" name = 'filename' size=10 required/></p>
 <p class = "sub"><input type = "submit" name = "submit" value = "submit my info" id ="buttonS" /></p>
  </fieldset>
 </form>
